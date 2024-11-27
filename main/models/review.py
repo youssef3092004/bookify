cb2618fa6a8376cdff09ad8aba64199af4469d3e
youssef <youssefahmedabdelkader25@gmail.com>
2024-11-27 20:25:38 +0200

    Added Review model with fields for rating, description, and timestamps
    - Created a Review model with fields for rating (1-5), description, and timestamps (created_at, updated_at).
    - Added to_dict method to return model data as a dictionary for easy serialization.
    - Defined a __str__ method to return a string representation of the review with the rating and description.

---
+++ main/models/review.py
@@ -0,0 +1,23 @@
+from django.db import models
+from uuid import uuid4
+from datetime import datetime
+
+class review(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    rating = models.IntegerField() #! Rating (1-5)
+    description = models.CharField(max_length=100)
+    created_at = models.DateTimeField(default=datetime.now)
+    updated_at = models.DateTimeField(default=datetime.now)
+
+    def to_dict(self):
+        """Convert the model instance to a dictionary."""
+        return {
+            "id": str(self.id),
+            "description": self.description,
+            "rating": self.rating,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at,
+        }
+
+    def __str__(self):
+        return f'({self.rating}⭐) {self.description}'
