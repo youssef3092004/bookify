5b847fb7c88d9afac14000903fad0cc114ff18ba
youssef <youssefahmedabdelkader25@gmail.com>
2024-11-30 17:33:20 +0200

    Update models to use timezone-aware DateTimeField
    - Ensured `created_at` and `updated_at` fields in models use `timezone.now`
      for timezone-aware datetime values.
    - Generated and applied migrations to reflect changes in the database schema.

--- main/models/discount_user_hotel.py
+++ main/models/discount_user_hotel.py
@@ -1,15 +1,14 @@
 from django.db import models
 from uuid import uuid4
-from datetime import datetime
+from django.utils import timezone
 
 class DiscountUserHotel(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     discount = models.ForeignKey('Discount', on_delete=models.CASCADE, related_name='discount_user_hotels')
     user = models.ForeignKey('User', on_delete=models.CASCADE, related_name='discount_user_hotels')
     hotel = models.ForeignKey('Hotel', on_delete=models.CASCADE, related_name='discount_user_hotels')
-    created_at = models.DateTimeField(default=datetime.now)
-    updated_at = models.DateTimeField(default=datetime.now)
-
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(default=timezone.now)
     def to_dict(self):
         """Convert the model instance to a dictionary."""
         return {
